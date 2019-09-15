eca0e31e67fbb889a19dccce9ce23ccd4c118813
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-15 10:39:44 +0100

    section15 reactive setup form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'section15-reactive-setup-form';
+  genders = ['male', 'female'];
 }
