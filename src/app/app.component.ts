2dbee441d6986d19f6a27145611c9d178bf61f46
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-15 15:26:55 +0100

    section15 reactive setup form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,13 +29,19 @@ export class AppComponent implements OnInit {
       (status) => console.log(status)
     );
 
-    this.signupForm.setValue({
+   this.signupForm.setValue({
       'userData': {
         'username': 'Max',
         'email': 'max@test.com'
       },
       'gender': 'male',
       'hobbies':[]
+    });
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Max',
+      },
     });
 
 
